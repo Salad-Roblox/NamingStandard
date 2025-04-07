f6d2df7408f14e047733d965f84470283f36afa1
Carot <174653796+ILiekCarot@users.noreply.github.com>
2025-04-07 14:34:53 +0200

    sallad

--- RENCCheckEnv.lua
+++ RENCCheckEnv.lua
@@ -1,6 +1,7 @@
 local testScript
 local testScriptType
 local _getsenv = getsenv or require
+if identifyexecutor() == "Salad" then _getsenv = require end
 for _, x in pairs(game:GetService("Players").LocalPlayer.PlayerScripts:GetDescendants()) do
 	if not testScript or ((x.ClassName == "ModuleScript" and type(_getsenv(x) or nil) == "table") and testScriptType == "LocalScript") then
 		testScript = x
